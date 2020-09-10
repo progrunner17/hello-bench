08a68cdf5cdfff9858eae1d9234fcaedc30d215a
progrunner17 <g.sho1500@gmail.com>
2020-09-10 17:38:11 +0900

    fix trailing space

--- hello.py
+++ hello.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 
 # The MIT License (MIT)
-# 
+#
 # Copyright (c) 2015 Tintri
-# 
+#
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
 # in the Software without restriction, including without limitation the rights
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
-# 
+#
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
-# 
+#
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -207,7 +207,7 @@ class BenchRunner:
         self.registry2 = registry2
         if self.registry2 != '':
             self.registry2 += '/'
-        
+
     def run_echo_hello(self, repo):
         cmd = '%s run %s%s echo hello' % (self.docker, self.registry, repo)
         rc = os.system(cmd)
